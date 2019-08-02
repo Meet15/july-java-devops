7c75daebcd71ed8a9a58b52e7a5502865ee9f336
Meet Patel <meetpatel@Meets-MacBook-Pro.local>
2019-08-01 21:02:38 -0700

    calculatorApp

--- TestClass.java
+++ TestClass.java
@@ -1,5 +1,45 @@
 package com.flexon.project;
 
-public class TestClass {
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Test;
+
+public class TestClass {
+	@Test
+	public void testSum() {
+		System.out.println("sum");
+		Integer x=15;
+		Integer y=15;
+		Integer expResult=30;
+		Integer result= Testapp.Sum(x,y);
+		assertEquals(expResult,result);
+	}
+	@Test
+	public void testSub() {
+		System.out.println("sub");
+		Integer x=15;
+		Integer y=15;
+		Integer expResult=30;
+		Integer result= Testapp.Sub(x,y);
+		assertEquals(expResult,result);
+	}
+	@Test
+	public void testMul() {
+		System.out.println("mul");
+		Integer x=15;
+		Integer y=15;
+		Integer expResult=30;
+		Integer result= Testapp.Mul(x,y);
+		assertEquals(expResult,result);
+	}
+	@Test
+	public void testDiv() {
+		System.out.println("div");
+		Integer x=15;
+		Integer y=15;
+		Integer expResult=30;
+		Integer result= Testapp.Div(x,y);
+		assertEquals(expResult,result);
+	}
+	
 }
--- Testapp.java
+++ Testapp.java
@@ -1,5 +1,26 @@
 package com.flexon.project;
 
 public class Testapp {
-
+	public static Integer Sum(Integer x,Integer y) {
+		if( x == null || y == null)
+			return null;
+		return x+ y;
+		
+	}
+	public static Integer Sub(Integer x,Integer y) {
+		if( x == null || y == null)
+			return null;
+		return x- y;
+	
+	}
+	public static Integer Mul(Integer x,Integer y) {
+		if( x == null || y == null)
+			return null;
+		return x* y;
+	}
+	public static Integer Div(Integer x,Integer y) {
+		if( x == null || y == null)
+			return null;
+		return x/ y;
+	}
 }
